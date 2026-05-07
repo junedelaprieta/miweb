0b46de39e80fe5977fa1fc3174b1a94ff392c896
junedelaprieta <65020420+junedelaprieta@users.noreply.github.com>
2026-05-07 09:57:15 +0200

    update1

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,8 @@ function openStream(user){
   const modal = document.getElementById('modal');
   const frame = document.getElementById('twitchFrame');
 
-  frame.src = `https://player.twitch.tv/?channel=${franbeuve}&parent=junedelaprieta.github.io`;
+  frame.src = `https://player.twitch.tv/?channel=${user}&parent=localhost&parent=127.0.0.1&parent=junedelaprieta.github.io`;
+
   modal.style.display = "flex";
 }
 
